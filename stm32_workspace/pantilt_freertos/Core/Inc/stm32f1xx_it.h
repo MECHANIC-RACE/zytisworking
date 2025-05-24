7909e248cc58bc48c764a79eed37c7a5416c908d
labbbbbbbbb <724757025@qq.com>
2025-05-25 03:48:01 +0800

    change uart

--- stm32_workspace/pantilt_freertos/Core/Inc/stm32f1xx_it.h
+++ stm32_workspace/pantilt_freertos/Core/Inc/stm32f1xx_it.h
@@ -58,6 +58,7 @@ void EXTI4_IRQHandler(void);
 void EXTI9_5_IRQHandler(void);
 void TIM1_UP_IRQHandler(void);
 void USART2_IRQHandler(void);
+void USART3_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
